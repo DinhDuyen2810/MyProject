0ba64e5c3a58d0e4ad7c3cb2ae67644b6dfff98c
DinhDuyen2810 <dinhduyen299792@gmail.com>
2025-04-20 22:11:51 +0700

    Remove AutoFill folder from main branch

--- AutoFill/background.js
+++
@@ -1,7 +0,0 @@
-// Background script có thể để trống hoặc thêm các sự kiện cần thiết
-console.log('Background script đang chạy');
-
-// Ví dụ: Xử lý khi extension được cài đặt
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension đã được cài đặt');
-});
